2b335bc46a191eef79f9bf22ffb8db0bdc716b2c
darren <darrenzhang200027@gmail.com>
2020-09-06 00:46:12 -0400

    added logic for music artist's top 10 song lyrics

---
+++ lyrics.py
@@ -0,0 +1,46 @@
+import configparser
+import requests
+
+def getAccessToken():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['Client_Access_Token']['token']
+
+token = getAccessToken()
+
+def searchMusicArtist(name):
+    api_url = "http://api.genius.com/search?q={}".format(name)
+    headers = {"authorization": token}
+    r = requests.get(api_url, headers=headers)
+    return r.json()
+
+def getArtistId(name):
+    r = searchMusicArtist(name)
+    id = r["response"]["hits"][0]["result"]["primary_artist"]["id"]
+    return id
+
+def getTopTenSongs(name):
+    id = getArtistId(name)
+    api_url = "http://api.genius.com/artists/{}/songs".format(id)
+    headers = {"authorization": token}
+    params = {
+        "sort": "popularity",
+        "per_page": 10
+    }
+    r = requests.get(api_url, headers=headers, params=params)
+    return r.json()
+
+def getLyricsArray(name):
+    r = getTopTenSongs(name)
+    songs = r["response"]["songs"]
+    lyrics_array = []
+    for song in songs:
+        lyrics_array.append(song["url"])
+    return lyrics_array
+
+
+
+
+print(getLyricsArray("Kendrick Lamar"))
+
+
