d055abfde78c3a19aaba09720ee79cb1b734cd78
darren <darrenzhang200027@gmail.com>
2020-09-08 19:54:17 -0400

    branch4

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markov import MarkovLyrics
 from lyrics import webScrapeLyrics
+
 
 # Create an instance of a Flask application
 app = Flask(__name__) #four underscores total
 
-# Our home route
-@app.route('/')
-def lyricsGenerator():
-    songs_array = webScrapeLyrics("Kendric Lamar")
+def generateArtistSong(name):
+    songs_array = webScrapeLyrics(name)
     m = MarkovLyrics()
     # Populate the markov chain using all 10 songs
     for song in songs_array:
         # Recall that each song contains an array of lyrics
         m.populateMarkovChain(song)
-    print(m.generateLyrics())    
+    return m.generateLyrics()
 
-    return render_template('home.html')
+# Our home route
+@app.route('/', methods = ['GET', 'POST'])
+def lyricsGenerator():
+    song = ""
+    if request.method == 'POST':
+        artist = request.form['search']
+        song = generateArtistSong(artist)
+    return render_template('home.html', song=song)
+
+
 
 if __name__ == '__main__':
     app.run()
