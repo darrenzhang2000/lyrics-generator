13b16bc5ecb3c56423ee2687262b96f2d0cc15fa
darren <darrenzhang200027@gmail.com>
2020-09-08 14:33:38 -0400

    added logic for generating lyrics

--- markov.py
+++ markov.py
@@ -2,25 +2,65 @@ from random import randint
 
 class MarkovLyrics:
     def __init__(self):
-        self.markov_lyrics = {}
-        self.order = 1
+        # A dictionary containing phrases (each phrase contains self.order number of words)
+        # Example 
+        # self.chain = {
+        #   "baby": [plays, sleeps, crawls, sleeps],
+        #   "plays": [toy, food],
+        # }
+        self.chain = {}
 
-    def generateMarkovChain(self, lyrics):
+        self.order = 1 # The number of words in each phrase
+
+    def populateMarkovChain(self, lyrics):
         for line in lyrics:
             words = line.split(" ")
+            # For every possible starting index of the phrase  
             for i in range(len(words) - self.order):
                 phrase = ""
-                for j in range(self.order):
+                # Builds phrase with the first j words
+                for j in range(self.order): 
                     phrase += words[i + j]
-                if phrase in self.markov_lyrics:
+                # If the phrase is already in the dictionary, 
+                # append next_word to the list of possible words
+                # for the phrase
+                if phrase in self.chain:
                     next_word = words[i + self.order]
-                    self.markov_lyrics[phrase].append(next_word)
+                    self.chain[phrase].append(next_word)
+                # First time this phrase occurs
+                # next_word is the only possible next word right now
                 else:
                     next_word = words[i + self.order]
-                    self.markov_lyrics[phrase] = [next_word]
+                    self.chain[phrase] = [next_word]
+
+
+    def generateLyrics(self, lyrics_length=500):
+        n = len(m.chain)
+        m_dict = m.chain
+
+        # Choose a random starting word
+        start_index = randint(0, n - self.order)
+        l = list(m.chain)
+        cur_word = l[start_index]
+
+        # result string
+        res = cur_word + " "
+
+        for _ in range(lyrics_length):
+            if cur_word not in m.chain:
+                next_index = randint(0, len(m.chain) - 1)
+                l = list(m.chain)
+                cur_word = l[next_index]
+            else:
+                possible_words = m_dict[cur_word]
+                index = randint(0, len(possible_words) - 1)
+                next_word = possible_words[index]
+                res += next_word + ' '
+                cur_word = next_word
+        return res
 
 m = MarkovLyrics()
-m.generateMarkovChain(['''Interference, it's such a receipt, huh?
+dummydata = ['''Interference, it's such a receipt, huh?
 
 I want
 
@@ -38,20 +78,9 @@ Would fight less and right
 
 And funeral faces
 
-James bonding with'''])
+James bonding with''']
+# dummydata = ['cat dog cat cat cat dog bat fat dat quat']
+m.populateMarkovChain(dummydata)
 
-n = len(m.markov_lyrics)
-
-m_dict = m.markov_lyrics
-
-start_index = randint(0, n)
-l = list(m.markov_lyrics)
-cur_word = l[start_index]
-res = cur_word + " "
-for i in range(10):
-    possible_words = m_dict[cur_word]
-    index = randint(0, len(possible_words) - 1)
-    next_word = possible_words[index]
-    res += next_word + ' '
-    cur_word = next_word
-print(res)
+print(m.generateLyrics())
+# print(m.chain)
