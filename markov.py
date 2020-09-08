240a011eb35748661c18bcc8497257a3b57ecf8e
darren <darrenzhang200027@gmail.com>
2020-09-08 15:24:08 -0400

    simplied order to 1

--- markov.py
+++ markov.py
@@ -7,80 +7,57 @@ class MarkovLyrics:
         # self.chain = {
         #   "baby": [plays, sleeps, crawls, sleeps],
         #   "plays": [toy, food],
+        #   ...
         # }
         self.chain = {}
-
-        self.order = 1 # The number of words in each phrase
 
     def populateMarkovChain(self, lyrics):
         for line in lyrics:
             words = line.split(" ")
-            # For every possible starting index of the phrase  
-            for i in range(len(words) - self.order):
-                phrase = ""
-                # Builds phrase with the first j words
-                for j in range(self.order): 
-                    phrase += words[i + j]
-                # If the phrase is already in the dictionary, 
+            # For every possible starting index of the word  
+            for i in range(len(words) - 1):
+                word = words[i]
+
+                # If the word is already in the dictionary, 
                 # append next_word to the list of possible words
-                # for the phrase
-                if phrase in self.chain:
-                    next_word = words[i + self.order]
-                    self.chain[phrase].append(next_word)
-                # First time this phrase occurs
+                # for the word
+                if word in self.chain:
+                    next_word = words[i + 1]
+                    self.chain[word].append(next_word)
+
+                # First time this word occurs
                 # next_word is the only possible next word right now
                 else:
-                    next_word = words[i + self.order]
-                    self.chain[phrase] = [next_word]
+                    next_word = words[i + 1]
+                    self.chain[word] = [next_word]
 
-
-    def generateLyrics(self, lyrics_length=500):
+    # length is the length so the song we generate
+    def generateLyrics(self, length=500):
         n = len(m.chain)
-        m_dict = m.chain
 
         # Choose a random starting word
-        start_index = randint(0, n - self.order)
-        l = list(m.chain)
-        cur_word = l[start_index]
+        start_index = randint(0, n - 1)
+        keys = list(m.chain.keys())
+        cur_word = keys[start_index]
 
         # result string
         res = cur_word + " "
 
-        for _ in range(lyrics_length):
+        for _ in range(length):
             if cur_word not in m.chain:
+                res += '\n'
                 next_index = randint(0, len(m.chain) - 1)
                 l = list(m.chain)
                 cur_word = l[next_index]
             else:
-                possible_words = m_dict[cur_word]
+                possible_words = m.chain[cur_word]
                 index = randint(0, len(possible_words) - 1)
                 next_word = possible_words[index]
                 res += next_word + ' '
                 cur_word = next_word
         return res
 
+data = ["cow dog bunny dog cow horse", "dog cat dog cat"]
 m = MarkovLyrics()
-dummydata = ['''Interference, it's such a receipt, huh?
-
-I want
-
-I drink liquor on they mom while she wanna hear not your ways deceitful
-
-Because you got a gang member, you's a destruction mode if phil jackson came from the same niggas just what happens on full, that's the one hand, I been ready, my metropolis, feel a song, alright
-
-Tapped in the blacker the weekend
-
-Show you want everything black, I know you killed
-
-You ain't good game, homie, you want
-
-Would fight less and right
-
-And funeral faces
-
-James bonding with''']
-# dummydata = ['cat dog cat cat cat dog bat fat dat quat']
-m.populateMarkovChain(dummydata)
-
+m.populateMarkovChain(data)
 print(m.generateLyrics())
-# print(m.chain)
